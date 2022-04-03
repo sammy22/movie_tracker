f7fa2815f13d1c6befed3b0d3caa647ef322e6bc
Niraj <niraj@Nirajs-MacBook-Air.local>
2022-04-02 23:37:13 -0600

    Button features

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,14 @@
+
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+var multer = require('multer');
+var upload = multer();
+
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(upload.array()); 
+
 
 app.use(express.static(__dirname + "/public"))
 app.use('/css', express.static(__dirname + 'public/css'))
@@ -17,6 +26,39 @@ app.get('', (req, res) => {
 
 app.get('/signup', (req, res) => {
     res.render('signup')
+})
+
+app.get('/test', (req, res) => {
+    res.render('test')
+})
+
+app.post('/register', (req,res)=>{
+    
+    var name = req.body.name;
+    var email =req.body.email;
+    var pass = req.body.password;
+    
+  
+    var data = {
+        "name": name,
+        "email":email,
+        "password":pass
+    }
+    console.info(data)
+    // return res.redirect('signup_success.html');
+})
+
+app.post('/signin', (req,res)=>{
+    var email =req.body.email;
+    var pass = req.body.password;
+    
+  
+    var data = {
+        "email":email,
+        "password":pass
+    }
+    console.info(data)
+    // return res.redirect('signup_success.html');
 })
 
 app.listen(port,  () => console.info("listening on ${port}"))

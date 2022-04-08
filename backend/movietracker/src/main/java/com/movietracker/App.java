72473cf4c79f730ef92e199b956ecd551fd61752
Joshua Sun <joshua.sun@colorado.edu>
2022-04-08 16:59:51 -0600

    signin signup post

--- backend/movietracker/src/main/java/com/movietracker/App.java
+++ backend/movietracker/src/main/java/com/movietracker/App.java
@@ -1,13 +1,40 @@
 package com.movietracker;
 
+import java.net.URL;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
 /**
- * Hello world!
- *
+ * Starts up a server that serves static files from the top-level directory.
+ * 
+ * from: https://happycoding.io/tutorials/java-server/embedded-jetty
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+  public static void main(String[] args) throws Exception {
+
+    // Create a server that listens on port 8080.
+    Server server = new Server(8080);
+    WebAppContext webAppContext = new WebAppContext();
+    server.setHandler(webAppContext);
+
+    // Load static content from the resources directory.
+
+    URL webAppDir =
+        App.class.getClassLoader().getResource("META-INF/resources");
+    webAppContext.setResourceBase(webAppDir.toURI().toString());
+
+    // // Look for annotations in the classes directory (dev server) and in the
+    // // jar file (live server).
+    webAppContext.setAttribute(
+        "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", 
+        ".*/target/classes/|.*\\.jar");
+
+    // Start the server! 🚀
+    server.start();
+    System.out.println("Server started!");
+
+    // Keep the main thread alive while the server is running.
+    server.join();
+  }
 }
